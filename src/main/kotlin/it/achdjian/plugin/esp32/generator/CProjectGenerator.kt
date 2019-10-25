ba0cf6bd40611973b011f53c67144f6e02c8ef53
paoloach <paolo.achdjian@skillbill.it>
2019-10-25 08:36:46 +0200

    Remove all useless cmake targets

--- src/main/kotlin/it/achdjian/plugin/esp32/generator/CProjectGenerator.kt
+++ src/main/kotlin/it/achdjian/plugin/esp32/generator/CProjectGenerator.kt
@@ -4,22 +4,33 @@ import com.intellij.execution.RunManagerEx
 import com.intellij.execution.impl.RunManagerImpl
 import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.cidr.cpp.cmake.CMakeSettings
 import com.jetbrains.cidr.cpp.cmake.projectWizard.generators.CMakeAbstractCProjectGenerator
 import com.jetbrains.cidr.cpp.cmake.projectWizard.generators.settings.CMakeProjectSettings
 import com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace
+import com.jetbrains.cidr.cpp.execution.CMakeAppRunConfigurationType
 import it.achdjian.plugin.esp32.configurations.flash.FlashConfigurationType
 import it.achdjian.plugin.esp32.configurations.flash.FlashRunConfiguration
 import it.achdjian.plugin.esp32.configurator.*
 import it.achdjian.plugin.esp32.entry_type.ConfigurationEntry
 import it.achdjian.plugin.esp32.setting.ESP32SettingState
 import it.achdjian.plugin.esp32.ui.getResourceAsString
+import java.util.concurrent.TimeoutException
 import javax.swing.JComponent
 
 class CProjectGenerator : CMakeAbstractCProjectGenerator() {
+    companion object {
+        val esp32Project: Key<Boolean> = Key.create("ESP32")
+        private val LOG = Logger.getInstance(CProjectGenerator::class.java)
+        private val allowedCmakeTarget = listOf("flash", "app","apidoc","partition_table")
+    }
+
+
     private val wizardData = WizardData()
 
     override fun getName(): String = "ESP32 C project"
@@ -30,13 +41,13 @@ class CProjectGenerator : CMakeAbstractCProjectGenerator() {
             val files = mutableListOf<VirtualFile>()
             createMainFile(path, files)
             return files.toTypedArray()
-        } else{
+        } else {
             return arrayOf()
         }
     }
 
     override fun getSettingsPanel(): JComponent? {
-        if (ESP32SettingState.validSDKPath()){
+        if (ESP32SettingState.validSDKPath()) {
             return ESP32WizardPanel(createSettingsPanel(), wizardData.entries)
         } else {
             return MissingConfig(createSettingsPanel())
@@ -51,20 +62,47 @@ class CProjectGenerator : CMakeAbstractCProjectGenerator() {
                 .replace("__{project_name}__", projectName)
                 .replace("__{SDK_PATH}__", ESP32SettingState.sdkPath)
             return cmakelists
-        } else{
+        } else {
             return ""
         }
     }
 
-    override fun generateProject(project: Project, path: VirtualFile, cmakeSetting: CMakeProjectSettings, module: Module) {
+    override fun generateProject(
+        project: Project,
+        path: VirtualFile,
+        cmakeSetting: CMakeProjectSettings,
+        module: Module
+    ) {
         if (ESP32SettingState.validSDKPath()) {
             super.generateProject(project, path, cmakeSetting, module)
             generateCrossCompilerConfiguration(project)
             generateFlashConfiguration(project)
+            project.putUserData(esp32Project, true)
+            removeCmakeConfiguration(project)
+
+
         }
     }
 
-    private fun  generateCrossCompilerConfiguration(project: Project){
+    private fun removeCmakeConfiguration(project: Project){
+        val cMakeWorkspace = CMakeWorkspace.getInstance(project)
+        ApplicationManager.getApplication().executeOnPooledThread {
+            try {
+                cMakeWorkspace.waitForReloadsToFinish(1000000)
+                val runManager = RunManagerEx.getInstanceEx(project) as RunManagerImpl
+                val toRemove = runManager
+                    .allSettings
+                    .filter { it.type is CMakeAppRunConfigurationType}
+                    .filter {  !allowedCmakeTarget.contains(it.name)}
+
+                runManager.removeConfigurations(toRemove)
+
+            } catch (e: TimeoutException) {
+                LOG.warn("workspace building too long")
+            }
+        }
+    }
+    private fun generateCrossCompilerConfiguration(project: Project) {
         val cMakeWorkspace = CMakeWorkspace.getInstance(project)
         val settings = cMakeWorkspace.settings
         val env = mapOf("PATH" to "${ESP32SettingState.crosscompilerPath}:/usr/bin:/sbin:/bin:/opt/bin")
@@ -84,13 +122,15 @@ class CProjectGenerator : CMakeAbstractCProjectGenerator() {
 
     private fun generateFlashConfiguration(project: Project) {
         val runManager = RunManagerEx.getInstanceEx(project) as RunManagerImpl
-        FlashConfigurationType.factory ?.let {
+        FlashConfigurationType.factory?.let {
             val newConfig = RunnerAndConfigurationSettingsImpl(
                 runManager,
                 FlashRunConfiguration(project, it, "Flash"),
                 false
             )
             runManager.addConfiguration(newConfig)
+
+            runManager.selectedConfiguration = newConfig
         }
 
     }
@@ -99,10 +139,10 @@ class CProjectGenerator : CMakeAbstractCProjectGenerator() {
 fun createMainFile(
     path: VirtualFile,
     files: MutableList<VirtualFile>
-){
+) {
 
     ApplicationManager.getApplication().runWriteAction {
-        val mainDir = path.findChild(MAIN_DIR)?: path.createChildDirectory(null, MAIN_DIR)
+        val mainDir = path.findChild(MAIN_DIR) ?: path.createChildDirectory(null, MAIN_DIR)
         val cMakeListsFile = mainDir.findOrCreateChildData(null, "CMakeLists.txt")
         val cMakeListsData = getResourceAsString("templates/main/CMakeLists.txt")
 
@@ -118,7 +158,7 @@ fun createMainFile(
 
 
 fun createSdkConfigFile(entries: List<ConfigurationEntry>, path: VirtualFile) {
-    val configurations = mutableListOf<Pair<String,String>>()
+    val configurations = mutableListOf<Pair<String, String>>()
     entries.forEach { it.addConfiguration(configurations) }
 
     val data = configurations.joinToString(separator = "\n") { "CONFIG_${it.first}=${it.second}" }
