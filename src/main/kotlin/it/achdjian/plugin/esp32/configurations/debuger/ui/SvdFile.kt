54e4c741a13bd58a579307d02e842c758748f20d
paoloach <paolo.achdjian@skillbill.it>
2020-07-31 20:37:24 +0200

    Remove svd classes Update version

--- src/main/kotlin/it/achdjian/plugin/esp32/configurations/debuger/ui/SvdFile.kt
+++
@@ -1,9 +0,0 @@
-package it.achdjian.plugin.esp32.configurations.debuger.ui
-
-import java.nio.file.Path
-
-class SvdFile(val path: Path) :  SvdNode<SvdFile>(path.toString(),path.toString()) {
-    override fun toString(): String {
-        return path.fileName?.toString() ?: path.toString()
-    }
-}
